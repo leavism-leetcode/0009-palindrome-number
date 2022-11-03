fc38b02489a48ad04a8809c92246b1fb6bb21a01
leavism <ghuydang@gmail.com>
2022-11-03 15:42:59 -0700

    Implemented a solution that reverses it with math. Ran into an interesting issue when hitting the max range of int

--- src/Solution.java
+++ src/Solution.java
@@ -42,11 +42,38 @@ public class Solution {
         return false;
     }
 
-    public static void main(String[] args) {
-        System.out.println(halfIsPalindrome(12521));
+    public static boolean mathIsPalindrome(int x) {
+        int length = String.valueOf(x).length();
+        int original = x;
+        int rem = 0;
+        double dec = Math.pow(10, length - 1);
+        while ( x > 0) {
+            rem += (x % 10) * dec;
+            dec /= 10;
+            x /= 10;
+        }
+        return rem == original;
+    }
 
-        // 12 3 21
-        // 123 321
-        // 123 123
+    public static void main(String[] args) {
+        System.out.println(mathIsPalindrome(
+                2147483647));
+
+        // 1234 || 0
+        // 123 R 4 || 4
+        // 123 R 3 || 34 43
+
+        // 12321 || 0
+        // 1232 R 1 || 1
+        // 123 R 2 || 21
+        // 12 R 3 || 321
+        // 1 R 2 || 2321
+        // 0 R 1 || 12321
+
+        // 1221
+        // 122 R 1
+        // 12 R 2
+        // 1 R 2
+        // 0 R 1
     }
 }
